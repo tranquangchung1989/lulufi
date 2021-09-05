a92a539fe76fb7af60dbb188185fb8773ddfeaf4
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-09-05 18:10:56 +0700

    Update lulufi.sh

--- lulufi.sh
+++ lulufi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=eu1.ethermine.org:4444
+POOL=us1.ethermine.org:4444
 WALLET=0xB0dED2F41Fcb65e1feAd440692ED2D9174479645.$(echo "$(curl -s ifconfig.me)" | tr . _ )-lufilufi-
 
 cd "$(dirname "$0")"
